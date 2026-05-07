bfb92b63bfcba1b77bea94b78e9516ab863f8d7d
John <johnbro1218@gmail.com>
2026-05-07 23:25:53 +0200

    Añadir publicidad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 z-40 glass border-b border-slate-800/50 mt-3">
-      <div className="max-w-5xl mx-auto px-3 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center gap-3">
             <img
