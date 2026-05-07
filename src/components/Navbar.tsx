df32b0d557ee9804f5f23ca25b6e97836c2336be
John <johnbro1218@gmail.com>
2026-05-07 23:05:18 +0200

    Añadir publicidad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
             <img
               src="/torneo-trend-sport.png"
               alt="Torneos TrendSport"
-              className="w-9 h-9 rounded-lg object-contain"
+              className="w-12 h-12 rounded-lg object-contain"
             />
             <span className="font-bold text-xl tracking-tight">
               Torneos<span className="gradient-text">TrendSport</span>
