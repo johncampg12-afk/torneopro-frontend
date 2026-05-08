0af21abdbf0f2a4c8a237af5e4e662840e9d828f
John <johnbro1218@gmail.com>
2026-05-08 18:14:15 +0200

    publi

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,8 +10,8 @@ const ads = [
   { id: 3, image: '/sponsors/EMPRENDE-CONMIGO-TREND-SPORT.png', link: '#' },
   { id: 4, image: '/sponsors/trend-sport-logo.jpeg', link: '#' },
   { id: 5, image: '/sponsors/dental.jpeg', link: '#' },
-  { id: 6, image: '/sponsors/igana.jpeg', link: '#' },
-  { id: 7, image: '/sponsors/ig.jpeg', link: '#' },
+  { id: 6, image: '/sponsors/igana.png', link: '#' },
+  { id: 7, image: '/sponsors/ig.png', link: '#' },
 ];
 
 export default function Home() {
